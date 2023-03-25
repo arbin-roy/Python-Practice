8ab6d376b39ce039850d620dd9b38e7b74d6b7c5
arbin-roy <sroy45597@gmail.com>
2023-03-26 00:54:37 +0530

    completed circular doubly linked list

--- DSA - Udemy/LinkedList/CircularDoublyLinkedList.py
+++ DSA - Udemy/LinkedList/CircularDoublyLinkedList.py
@@ -47,6 +47,81 @@ class CircularDoublyLinkedList:
                 tempNode.next.prev = newNode
                 tempNode.next = newNode
 
+    def traverseForwardCDLL(self):
+        if self.head is None:
+            return "LinkedList is empty!"
+        node = self.head
+        while node:
+            print(node.value)
+            node = node.next
+            if node == self.head:
+                break
+        return 0
+
+    def traverseBackwardCDLL(self):
+        if self.head is None:
+            return "LinkedList is empty!"
+        node = self.tail
+        while node:
+            print(node.value)
+            node = node.prev
+            if node == self.tail:
+                break
+        return 0
+
+    def searchCDLL(self, searchValue):
+        if self.head is None:
+            return "LinkedList is empty!"
+        node = self.head
+        while node:
+            if node.value == searchValue:
+                return "Value found!"
+            if node.next == self.head:
+                return "404"
+            node = node.next
+
+    def deleteCDLL(self, position):
+        if self.head is None:
+            return "LinkedList is empty!"
+        else:
+            if self.head == self.tail:
+                self.head.prev = self.head.next = None
+                self.head = self.tail = None
+                return [n.value for n in self]
+            elif position == 0:
+                node = self.head
+                node.next.prev = self.tail
+                self.head = node.next
+                self.tail.next = self.head
+                return [n.value for n in self]
+            elif position == -1:
+                node = self.tail
+                node.prev.next = self.head
+                self.tail = node.prev
+                self.head.prev = self.tail
+                return [n.value for n in self]
+            else:
+                node = self.head
+                index = 0
+                while index < position - 1:
+                    node = node.next
+                    index += 1
+                node.next = node.next.next
+                node.next.prev = node
+                return [n.value for n in self]
+
+    def deleteEntireCDLL(self):
+        if self.head is None:
+            return "Nothing to delete if the list is empty!"
+        else:
+            self.tail.next = None
+            node = self.head
+            while node:
+                node.prev = None
+                node = node.next
+            self.head = self.tail = None
+            return [n.value for n in self]
+
 
 circularDoublyLinkedList = CircularDoublyLinkedList(0)
 circularDoublyLinkedList.insertCDLL(1, -1)
@@ -55,4 +130,8 @@ circularDoublyLinkedList.insertCDLL(3, -1)
 circularDoublyLinkedList.insertCDLL(4, -1)
 circularDoublyLinkedList.insertCDLL(5, -1)
 print([n.value for n in circularDoublyLinkedList])
-print(circularDoublyLinkedList.tail.value)
+# circularDoublyLinkedList.traverseForwardCDLL()
+# circularDoublyLinkedList.traverseBackwardCDLL()
+# print(circularDoublyLinkedList.searchCDLL(6))
+# print(circularDoublyLinkedList.deleteCDLL(7))
+print(circularDoublyLinkedList.deleteEntireCDLL())
